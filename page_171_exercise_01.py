d3fd9db3aec5e329389c0f3d7b8909fdcc62b8b2
Ngô Văn Úc <83104628+ngovanuc@users.noreply.github.com>
2021-10-06 22:47:50 +0700

    Add files via upload

---
+++ page_171_exercise_01.py
@@ -0,0 +1,16 @@
+"""
+author : Ngô Văn Úc
+date: 30/08/2021
+program: 1. Anne complains that defining functions to use in her programs is a lot of extra
+work. She says she can finish her programs much more quickly if she just writes
+them using the basic operators and control statements. State three reasons why her
+view is shortsighted.
+solution:
+    - lí do mà cô ấy có thể khẳng định được như vậy là vì trong chương trình chúng ta có thể tự định ước trước được
+    số xấp xỉ mà vẫn đảm bảo sự sai số không chênh lệch quá lớn dẫn đến sai số quá lớn.
+    bởi vì mặc dù chúng ta nhập vào tham số xấp xỉ thì chương trình cũng không hề quá phụ thuộc vào nó
+    tức là bản thân chương trình có thể tự quy định được.
+    hơn nữa tham số ước tính cũng không cần nhập vào, rõ ràng chúng ta có thể viết chương trình
+    đưa ra một só gần đúng, tuy nhiên việc quy định một số ban đầu dù không gần đúng nhưng chương trình vẫn có thể trả về
+    một kết quả tương đối như trong mong muốn.
+"""
